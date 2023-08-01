d88ad8cfe5906c9917e9168dede0cf5f5aaa927e
Jinseok Heo <jinseok.heo@sjsu.edu>
2023-08-01 14:22:14 -0700

    Edit Property Rent Accordion

--- src/components/PropertyRentFlow/PropertyRentAccordionView.jsx
+++ src/components/PropertyRentFlow/PropertyRentAccordionView.jsx
@@ -20,16 +20,21 @@ function PropertyRentAccordion(props) {
     const [status] = props.status;
     const [accordionCount, setAccordionCount] = useState(0);
     return (
-        <Accordion theme={theme} style={{ backgroundColor: getStatusColor(status), "font-family": 'Source Sans Pro', color: "#FFFFFF" }}>
+        <Accordion theme={theme} style={{ backgroundColor: getStatusColor(status), "font-family": 'Source Sans Pro', color: "#FFFFFF"}}>
             <AccordionSummary
                 expandIcon={
-                    <div className="property-rent-property-status-summary-text">
-                        {accordionCount}
-                        <ExpandMoreIcon />
-                    </div>
+                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M3.5 5.25L7 8.75L10.5 5.25" stroke="#F2F2F2" stroke-width="2.5" />
+                    </svg>
                 }
+                className="property-rent-property-status-summary-text-container"
             >
-                {status}
+                <div className="property-rent-property-status-summary-text-1">
+                    {status}
+                </div>
+                <div className="property-rent-property-status-summary-text-2">
+                    {accordionCount}
+                </div>
             </AccordionSummary>
             <AccordionDetails>
                 <PropertyRentAccordionCard data={["103 N. Abel St, Milpitas CA 95035", "$2300"]} />
