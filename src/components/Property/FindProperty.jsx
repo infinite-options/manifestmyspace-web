ab090794f109b5476d9ed1a65724910f123c2704
Nisarg Bhalavat <nisargbhalavat98@gmail.com>
2023-08-28 03:05:02 -0700

    property images

--- src/components/Property/FindProperty.jsx
+++ src/components/Property/FindProperty.jsx
@@ -301,13 +301,7 @@ const FindProperty = (props) => {
                         </Typography>
                     </Stack>
                     {propertyData.map((property, index) => {
-                        return (
-                            <PropertyCard
-                                data={property}
-                                key={index}
-                                images={images}
-                            />
-                        );
+                        return <PropertyCard data={property} key={index} />;
                     })}
                 </Paper>
             </Box>
@@ -320,24 +314,27 @@ function PropertyCard(props) {
 
     const property = props.data;
 
-    // const ppt_images = property.property_images
-    //     .replace('[', '')
-    //     .replace(']', '')
-    //     .replace('\\', '')
-    //     .replace('"', "'")
-    //     .replace('\n', '')
-    //     .replace(' ', '')
-    //     .split(',');
+    const ppt_images = property.property_images.split(',');
 
-    // const images = ppt_images.map((data) => {
-    //     try {
-    //         const url = new URL(data);
-    //         return { original: url };
-    //     } catch (e) {
-    //         console.error(e);
-    //         return { original: '' };
-    //     }
-    // });
+    function parseImageData(data) {
+        if (data === undefined) {
+            return;
+        }
+        const s = data.indexOf('http');
+        const l = data.indexOf('"', s);
+        const imageString = data.slice(s, l);
+        return imageString;
+    }
+
+    const images = ppt_images.map((data) => {
+        try {
+            const url = parseImageData(data);
+            return { original: url };
+        } catch (e) {
+            console.error(e);
+            return { original: '' };
+        }
+    });
 
     // console.log(images);
 
@@ -360,11 +357,12 @@ function PropertyCard(props) {
     return (
         <Card sx={{ margin: 5 }}>
             <ReactImageGallery
-                items={props.images}
+                items={images}
                 showFullscreenButton={false}
                 showPlayButton={false}
                 showThumbnails={false}
             />
+
             <Box
                 sx={{
                     backgroundColor: '#8897BA',
