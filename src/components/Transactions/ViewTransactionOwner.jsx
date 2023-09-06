b84854a25d0e65d03b081920a560b35cf9748f3b
Priyanka Cornelius <priyankajohn1997@gmail.com>
2023-09-06 13:46:09 -0700

    Added add expense.revenue and utility endpoints for cashflow, updated transaction history and dashboard components

--- src/components/Transactions/ViewTransactionOwner.jsx
+++ src/components/Transactions/ViewTransactionOwner.jsx
@@ -7,6 +7,7 @@ import {
     Button,
     Typography,
     Stack,
+    Grid
   } from "@mui/material";
 import MobileStepper from '@mui/material/MobileStepper';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
@@ -17,7 +18,13 @@ import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
 import theme from "../../theme/theme";
 import UTurnLeftIcon from '@mui/icons-material/UTurnLeft';
-
+import PayPal from '../../images/PayPal.png'
+import Zelle from '../../images/Zelle.png'
+import Venmo from '../../images/Venmo.png'
+import Chase from '../../images/Chase.png'
+import Stripe from '../../images/Stripe.png'
+import ApplePay from '../../images/ApplePay.png'
+import moment from 'moment';
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const images = [
@@ -240,24 +247,28 @@ export default function ViewTransactionOwner(props) {
                             <Stack
                             direction="row"
                             justifyContent="center"
+                            marginTop='30px'
                             >
-                                <Typography sx={{color: theme.typography.primary.black, fontWeight: theme.typography.primary.fontWeight, fontSize:theme.typography.largeFont}}>
+                                <Typography sx={{color: theme.typography.primary.black, fontWeight: theme.typography.primary.fontWeight, fontSize:24}}>
                                 ${txn.total_paid}
                                 </Typography>
                             </Stack>
                             <Stack
                             direction="row"
                             justifyContent="center"
-                            marginTop='10px'
+                            marginTop='30px'
                             >
-                                <Typography sx={{color: theme.typography.primary.black, fontSize:theme.typography.smallFont}}>
-                                Date Paid: {txn.purchase_date}
+                                <Typography sx={{color: theme.typography.primary.black, fontSize:16}}>
+                                Date Paid: {' '}
+                                {txn.purchase_date !== null
+                                                ? moment(txn.purchase_date.substring(0, 10)).format('MM/DD/YY')
+                                                : "Not Available"}
                                 </Typography>
                             </Stack>
                             <Stack
                             direction="row"
                             justifyContent="center"
-                            marginTop='10px'
+                            marginTop='30px'
                             >
                                 <Typography sx={{color: theme.typography.primary.black, fontWeight: theme.typography.primary.fontWeight, fontSize:theme.typography.largeFont}}>
                                 {txn.purchase_type}
@@ -266,33 +277,47 @@ export default function ViewTransactionOwner(props) {
                             <Stack
                             direction="row"
                             justifyContent="center"
-                            marginTop='10px'
+                            marginTop='30px'
                             >
-                                <Typography sx={{color: theme.typography.common.blue, fontSize:theme.typography.smallFont}}>
+                                <Typography sx={{color: theme.typography.common.blue, fontSize:15}}>
                                 {txn.pur_notes}
                                 </Typography>
                             </Stack>
                             <Stack
                             // direction="row"
                             justifyContent="center"
-                            marginTop='10px'
+                            margin='30px'
                             >
-                                <Stack
-                                direction="row"
-                                justifyContent="center"
-                                >
-                                    <Typography sx={{color: theme.typography.common.black, fontSize:theme.typography.smallFont}}>
-                                    {txn.payment_type}:{txn.total_paid}
-                                    </Typography>
-                                </Stack>
-                                <Stack
-                                direction="row"
-                                justifyContent="center"
-                                >
-                                    <Typography sx={{color: theme.typography.common.black, fontSize:theme.typography.smallFont}}>
-                                    {txn.pay_charge_id}
-                                    </Typography>
-                                </Stack>                        
+                                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                                    <Grid item xs={3}>
+                                    {txn.payment_type === 'STRIPE' && <img src={Stripe} alt='Stripe Logo' width='50' height='40' />}
+                                    {txn.payment_type === 'PAYPAL' && <img src={PayPal} alt='PayPal Logo' width='50' height='40' />}
+                                    {txn.payment_type === 'VENMO' && <img src={Venmo} alt='Venmo Logo' width='50' height='40' />}
+                                    {txn.payment_type === 'ZELLE' && <img src={Zelle} alt='Zelle Logo' width='50' height='40' />}
+                                    {txn.payment_type === 'CHASE' && <img src={Chase} alt='Chase Logo' width='50' height='40' />}
+                                    {txn.payment_type === 'APPLE PAY' && <img src={ApplePay} alt='ApplePay Logo' width='50' height='40' />}
+                                    {/* {txn.payment_type === 'WELLS FARGO' && <img src={ApplePay} alt='ApplePay Logo' width='50' height='40' />}
+                                    {txn.payment_type === 'BANK OF AMERICA' && <img src={ApplePay} alt='ApplePay Logo' width='50' height='40' />} */}
+                                    </Grid>
+                                    <Grid item xs={9}>
+                                        <Stack
+                                        direction="row"
+                                        justifyContent="left"
+                                        >
+                                            <Typography sx={{color: theme.typography.common.black, fontSize:14}}>
+                                            {txn.payment_type}: ${txn.total_paid}
+                                            </Typography>
+                                        </Stack>
+                                        <Stack
+                                        direction="row"
+                                        justifyContent="left"
+                                        >
+                                            <Typography sx={{color: theme.typography.common.black, fontSize:14}}>
+                                            {txn.pay_charge_id}
+                                            </Typography>
+                                        </Stack>   
+                                    </Grid>
+                                </Grid>                     
                             </Stack>
                             
                             <Box
@@ -301,13 +326,13 @@ export default function ViewTransactionOwner(props) {
                             display="flex"
                             justifyContent="space-between"
                             alignItems="center"
-                            marginTop='10px'
+                            marginTop='30px'
                             >
-                                <Typography sx={{color: theme.typography.common.blue, fontSize:theme.typography.smallFont}}>
-                                Transaction ID: 133
+                                <Typography sx={{color: theme.typography.common.blue, fontSize:12}}>
+                                Transaction ID: {txn.purchase_uid}
                                 </Typography>
-                                <Typography sx={{color: theme.typography.common.blue, fontSize:theme.typography.smallFont}}>
-                                Recipient ID: 123
+                                <Typography sx={{color: theme.typography.common.blue, fontSize:12}}>
+                                Recipient ID: {txn.receiver_user_id}
                                 </Typography>
                             </Box>
                         </Paper>
