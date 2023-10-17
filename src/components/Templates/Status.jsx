e525e61b7a3eba0b8676fdccd3ee326d04d59594
Aditya Baravkar <aditya.baravkar@sjsu.edu>
2023-10-17 09:50:08 -0700

    Owner dashboard fixes

--- src/components/Templates/Status.jsx
+++ src/components/Templates/Status.jsx
@@ -1,17 +1,22 @@
-import { useState } from "react"
-
-export default function Status({colorStatus, maintenanceStatusCount}){
-
-    console.log("status maintenanceRequests", maintenanceStatusCount)
+export default function Status({colorStatus, maintenanceRequests, selectedRole}){
+    console.log("status maintenanceRequests", maintenanceRequests)
     console.log("status colorStatus", colorStatus)
     return(
         <div id="mt-all-requests">
             <ul className="mt-widget-requests">
-                {colorStatus.map((item, index) => 
-                    <li key={index} style={{ backgroundColor: item.color, color: '#FFFFFF', fontFamily: 'Source Sans Pro', fontSize: '12px', fontWeight:600 }}>
-                        {item.status} <span style={{float: "right"}}>{maintenanceStatusCount[item.mapping]}</span>
-                    </li>
-                )}
+                {colorStatus.map((item, index) => {
+                    let status = item.mapping;
+                    if(selectedRole === "OWNER") {
+                        if (item.mapping === "NEW REQUEST") status = "NEW";
+                        if (item.mapping === "INFO REQUESTED") status = "INFO";
+                    }
+                    const num = maintenanceRequests[status]
+                    return (
+                        <li key={index} style={{ backgroundColor: item.color, color: '#FFFFFF', fontFamily: 'Source Sans Pro', fontSize: '12px', fontWeight:600 }}>
+                            {item.status} <span style={{float: "right"}}>{num}</span>
+                        </li>
+                    )
+                })}
             </ul>
         </div>
     )
