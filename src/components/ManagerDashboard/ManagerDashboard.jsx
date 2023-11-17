911402a85025c3c045298765fe27248c9233e60a
Ranjit Marathay <rmarathay@gmail.com>
2023-11-17 09:07:13 -0800

    fix PM rent dashboard

--- src/components/ManagerDashboard/ManagerDashboard.jsx
+++ src/components/ManagerDashboard/ManagerDashboard.jsx
@@ -186,54 +186,7 @@ function ManagerDashboard() {
                 <div className="mt-widgest-main">
                     <div className="mt-container">
                         <MaintenanceWidget selectedRole={"MANAGER"} maintenanceData={maintenanceStatusData}/>
-                        {/* <PropertyRentWidget {...propsForPropertyRentWidget}/> */}
-                        <div className="mt-prop-widget-container" onClick={() => navigate("/pmRent")}>
-                            <h2 className="mt-prop-widget-title"> Property Rent</h2>
-                            <div className="mt-prop-widget-graph">
-                                <PieChart width={200} height={250} >
-                                    <Legend
-                                        height={36}
-                                        iconType="circle"
-                                        layout="vertical"
-                                        verticalAlign="bottom"
-                                        iconSize={5}
-                                        padding={5}
-                                        formatter={renderColorfulLegendText}
-                                    />
-                                    <Pie
-                                        data={data}
-                                        cx={80}
-                                        cy={100}
-                                        innerRadius={35}
-                                        outerRadius={50}
-                                        paddingAngle={0}
-                                        dataKey="number"
-                                    >
-                                        {data.map((entry, index) => (
-                                            <Cell key={`cell-${index}`} fill={entry.fill} />
-                                        ))}
-                        
-                                    </Pie>
-                                    <text
-                                        x={85}
-                                        y={100}
-                                        textAnchor="middle"
-                                        dominantBaseline="middle"
-                                        cursor="pointer"
-                                        style={{
-                                            fontFamily: 'Source Sans Pro',
-                                            fontSize: '9px',
-                                            fill: '#160449',
-                                            fontWeight: '600',
-                                        }}
-                                        onClick={(e) => { e.stopPropagation(); navigate('/PMProperties') }}
-                                    >
-                                        View All {totalPropertiesCount}
-                                        <tspan x={85} y={110}>properties</tspan>
-                                    </text>
-                                </PieChart>
-                            </div>
-                        </div>
+                        <PropertyRentWidget {...propsForPropertyRentWidget}/>
                     </div>
                     <div className="mt-widget-expiry" onClick={() => navigate("/pmLeases")}>
                         {/* <div className="mt-expiry-container"> */}
