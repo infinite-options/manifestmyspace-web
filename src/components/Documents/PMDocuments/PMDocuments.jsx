1a508b5057ccbcd1fbba6c3baa5b319d5467d69b
rohitkphilip <rohitkurian.philip@sjsu.edu>
2024-01-22 22:55:02 -0800

    fixed Applications and Contracts in pmDocuments

--- src/components/Documents/PMDocuments/PMDocuments.jsx
+++ src/components/Documents/PMDocuments/PMDocuments.jsx
@@ -43,8 +43,8 @@ function PMDocuments() {
 
     useEffect(() => {
         setShowSpinner(true);
-        axios.get(`https://l0h6a9zi1e.execute-api.us-west-1.amazonaws.com/dev/ownerDocuments/${getProfileId()}`)
-        // axios.get(`http://localhost:4000/documents/${getProfileId()}`)
+        // axios.get(`https://l0h6a9zi1e.execute-api.us-west-1.amazonaws.com/dev/documents/${getProfileId()}`)
+        axios.get(`http://localhost:4000/documents/${getProfileId()}`) //rohit
         // axios.get(`http://localhost:4000/documents/600-000051`)
             .then((res) => {
                 // console.log(res.data);
@@ -55,7 +55,7 @@ function PMDocuments() {
                 setShowSpinner(false);
             });
     }, []);    
-    
+
     return (
         <>
             <Backdrop
@@ -232,6 +232,12 @@ function PMDocuments() {
                                 {contractsData.map((document) => {                                    
                                     const address = document.property_address;
                                     const unit = document.property_unit;
+                                    const city = document.property_city;
+                                    const zip = document.property_zip;
+                                    const state = document.property_state;
+                                    const owner_name = document.owner_first_name + " " + document.owner_last_name;
+                                    const owner_contact = document.owner_phone_number;
+                                    const owner_email = document.owner_email;
                                     let start_date = '';
                                     let end_date = '';
                                     let docs = [];
@@ -250,13 +256,13 @@ function PMDocuments() {
                                     return (
                                         <>
                                             {docs.map((doc) => (
-                                                <DocumentCard document={doc} data={[address, unit, start_date, end_date]} />
+                                                <DocumentCard document={doc} data={[address, unit, city, zip, state, start_date, end_date, owner_name, owner_contact, owner_email]} tab="Contracts"/>
                                             ))}
                                         </>
                                     )
                                 })}
 
-                                {applicationsData.map((document) => {                                    
+                                {/* {applicationsData.map((document) => {                                    
                                     const address = document.property_address;
                                     const unit = document.property_unit;
                                     let start_date = '';
@@ -277,18 +283,46 @@ function PMDocuments() {
                                     }
                                     return (
                                         <>
-                                            {docs.map((doc) => (
+                                            {applicationsData.map((doc) => (
                                                 <DocumentCard document={doc} data={[address, unit, start_date, end_date]} />
                                             ))}
                                         </>
                                     )
+                                })} */}
+
+                                {applicationsData.map((app) => {                                    
+                                    const address = app.property_address;
+                                    const unit = app.property_unit;
+                                    const city = app.property_city;
+                                    const zip = app.property_zip;
+                                    const state = app.property_state;
+                                    const start_date = app.lease_start;
+                                    const end_date = app.lease_end;
+                                    const applicant = app.tenant_first_name + " " + app.tenant_last_name;
+                                    const num_adults = JSON.parse(app.lease_adults).length;                                     
+
+
+                                    
+                                    switch (statusList[tabStatus]) {                                        
+                                        case 'Applications':
+                                            return (
+                                                <ApplicationCard data={[address, unit, city, state, zip, start_date, end_date, applicant, num_adults]} />
+                                            )                                                        
+                                            
+                                        default:
+                                            return null;
+                                    }
+                                    
                                 })}
 
                                 {leasesData.map((document) => {                                    
-                                    const address = document.property_address;
-                                    const unit = document.property_unit;
+                                    const address  = document.property_address;
+                                    const unit     = document.property_unit;
+                                    const city     = document.property_city;
+                                    const state    = document.property_state;
+                                    const zip      = document.property_zip;
                                     let start_date = '';
-                                    let end_date = '';
+                                    let end_date   = '';
                                     let docs = [];
                                     switch (statusList[tabStatus]) {                                        
                                         case 'Leases':
@@ -305,7 +339,7 @@ function PMDocuments() {
                                     return (
                                         <>
                                             {docs.map((doc) => (
-                                                <DocumentCard document={doc} data={[address, unit, start_date, end_date]} />
+                                                <DocumentCard document={doc} data={[address, unit, city, state, zip, start_date, end_date]} tab="Leases"/>
                                             ))}
                                         </>
                                     )
@@ -341,7 +375,169 @@ function NavTab(props) {
 
 function DocumentCard(props) {
     const document = props.document;
-    const [address, unit, start_date, end_date] = props.data;
+    const [address, unit, city, state, zip, start_date, end_date, owner_name, owner_contact, owner_email] = props.data;
+    const tab = props.tab;
+    const navigate = useNavigate();
+
+    function downloadDocument(url) {
+        window.open(url, '_newtab');
+    }
+    function navigateToDocument(url, link) {
+        navigate(url, {
+            state: {
+                link: link,
+            }
+        });
+    }
+    if(tab === "Leases"){
+        return (
+            <Box sx={{
+                backgroundColor: '#D6D5DA',
+                borderRadius: '10px',
+                padding: '5px',
+                marginBottom: '10px',
+                fontSize: '13px',
+            }}>
+                <Box sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                }}>
+                    <Box sx={{
+                        fontWeight: 'bold',
+                    }}>
+                        {document.filename}
+                    </Box>
+                    <Box sx={{
+                        fontWeight: 'bold',
+                    }}>
+                        Document type: {document.type}
+                    </Box>
+                    <Box sx={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                    }}>
+                        <Box
+                            sx={{
+                                marginRight: '3px',
+                            }}
+                            onClick={() => { downloadDocument(document.link) }}
+                        >
+                            <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M7 10.5L2.625 6.125L3.85 4.85625L6.125 7.13125V0H7.875V7.13125L10.15 4.85625L11.375 6.125L7 10.5ZM1.75 14C1.26875 14 0.856626 13.8285 0.513626 13.4855C0.170626 13.1425 -0.000581848 12.7307 1.48557e-06 12.25V9.625H1.75V12.25H12.25V9.625H14V12.25C14 12.7312 13.8285 13.1434 13.4855 13.4864C13.1425 13.8294 12.7307 14.0006 12.25 14H1.75Z" fill="#3D5CAC" />
+                            </svg>
+                        </Box>
+                        <Box>
+                        {/* <Box onClick={() => { navigateToDocument('/ownerDocumentsPDF', document.link) }}> */}
+                            <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M8.5 10.625C9.67361 10.625 10.625 9.67361 10.625 8.5C10.625 7.32639 9.67361 6.375 8.5 6.375C7.32639 6.375 6.375 7.32639 6.375 8.5C6.375 9.67361 7.32639 10.625 8.5 10.625Z" fill="#3D5CAC" />
+                                <path d="M16.4369 8.31938C15.812 6.70313 14.7273 5.30539 13.3167 4.29892C11.9062 3.29245 10.2316 2.72137 8.5 2.65625C6.7684 2.72137 5.09383 3.29245 3.68326 4.29892C2.2727 5.30539 1.18796 6.70313 0.563124 8.31938C0.520925 8.43609 0.520925 8.56391 0.563124 8.68062C1.18796 10.2969 2.2727 11.6946 3.68326 12.7011C5.09383 13.7075 6.7684 14.2786 8.5 14.3438C10.2316 14.2786 11.9062 13.7075 13.3167 12.7011C14.7273 11.6946 15.812 10.2969 16.4369 8.68062C16.4791 8.56391 16.4791 8.43609 16.4369 8.31938ZM8.5 11.9531C7.81704 11.9531 7.14941 11.7506 6.58155 11.3712C6.01368 10.9917 5.57109 10.4524 5.30973 9.82145C5.04837 9.19048 4.97999 8.49617 5.11322 7.82633C5.24646 7.15649 5.57534 6.5412 6.05827 6.05827C6.5412 5.57534 7.15649 5.24647 7.82633 5.11323C8.49617 4.97999 9.19048 5.04837 9.82145 5.30973C10.4524 5.57109 10.9917 6.01368 11.3712 6.58155C11.7506 7.14941 11.9531 7.81704 11.9531 8.5C11.9517 9.41539 11.5875 10.2929 10.9402 10.9402C10.2929 11.5875 9.41539 11.9517 8.5 11.9531Z" fill="#3D5CAC" />
+                            </svg>
+                        </Box>
+                    </Box>
+                </Box>
+                <Box>
+                    {`${address}, ${unit}`}
+                </Box>
+                <Box sx={{
+                    display: 'grid',
+                    gridTemplateColumns: 'repeat(2, 1fr)',
+                }}>
+                    <Box>
+                        Created: {start_date}
+                    </Box>
+                    <Box>
+                        By: {"<Doolittle Managements>"}
+                    </Box>
+                    <Box>
+                        Expires: {end_date}
+                    </Box>
+                    <Box>
+                        For: {"<Kim Gordon>"}
+                    </Box>
+                </Box>
+            </Box>
+        );
+    } else if(tab === "Contracts"){
+        return (
+            <Box sx={{
+                backgroundColor: '#D6D5DA',
+                borderRadius: '10px',
+                padding: '5px',
+                marginBottom: '10px',
+                fontSize: '13px',
+            }}>
+                <Box sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                }}>
+                    <Box sx={{
+                        fontWeight: 'bold',
+                    }}>
+                        {document.filename}
+                    </Box>
+                    <Box sx={{
+                        fontWeight: 'bold',
+                    }}>
+                        Document type: {document.type}
+                    </Box>
+                    <Box sx={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                    }}>
+                        <Box
+                            sx={{
+                                marginRight: '3px',
+                            }}
+                            onClick={() => { downloadDocument(document.link) }}
+                        >
+                            <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M7 10.5L2.625 6.125L3.85 4.85625L6.125 7.13125V0H7.875V7.13125L10.15 4.85625L11.375 6.125L7 10.5ZM1.75 14C1.26875 14 0.856626 13.8285 0.513626 13.4855C0.170626 13.1425 -0.000581848 12.7307 1.48557e-06 12.25V9.625H1.75V12.25H12.25V9.625H14V12.25C14 12.7312 13.8285 13.1434 13.4855 13.4864C13.1425 13.8294 12.7307 14.0006 12.25 14H1.75Z" fill="#3D5CAC" />
+                            </svg>
+                        </Box>
+                        <Box>
+                        {/* <Box onClick={() => { navigateToDocument('/ownerDocumentsPDF', document.link) }}> */}
+                            <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M8.5 10.625C9.67361 10.625 10.625 9.67361 10.625 8.5C10.625 7.32639 9.67361 6.375 8.5 6.375C7.32639 6.375 6.375 7.32639 6.375 8.5C6.375 9.67361 7.32639 10.625 8.5 10.625Z" fill="#3D5CAC" />
+                                <path d="M16.4369 8.31938C15.812 6.70313 14.7273 5.30539 13.3167 4.29892C11.9062 3.29245 10.2316 2.72137 8.5 2.65625C6.7684 2.72137 5.09383 3.29245 3.68326 4.29892C2.2727 5.30539 1.18796 6.70313 0.563124 8.31938C0.520925 8.43609 0.520925 8.56391 0.563124 8.68062C1.18796 10.2969 2.2727 11.6946 3.68326 12.7011C5.09383 13.7075 6.7684 14.2786 8.5 14.3438C10.2316 14.2786 11.9062 13.7075 13.3167 12.7011C14.7273 11.6946 15.812 10.2969 16.4369 8.68062C16.4791 8.56391 16.4791 8.43609 16.4369 8.31938ZM8.5 11.9531C7.81704 11.9531 7.14941 11.7506 6.58155 11.3712C6.01368 10.9917 5.57109 10.4524 5.30973 9.82145C5.04837 9.19048 4.97999 8.49617 5.11322 7.82633C5.24646 7.15649 5.57534 6.5412 6.05827 6.05827C6.5412 5.57534 7.15649 5.24647 7.82633 5.11323C8.49617 4.97999 9.19048 5.04837 9.82145 5.30973C10.4524 5.57109 10.9917 6.01368 11.3712 6.58155C11.7506 7.14941 11.9531 7.81704 11.9531 8.5C11.9517 9.41539 11.5875 10.2929 10.9402 10.9402C10.2929 11.5875 9.41539 11.9517 8.5 11.9531Z" fill="#3D5CAC" />
+                            </svg>
+                        </Box>
+                    </Box>
+                </Box>
+                <Box>
+                    {`${address}, ${city}, ${state}, ${zip}`}
+                </Box>
+                <Box sx={{
+                    display: 'grid',
+                    gridTemplateColumns: 'repeat(1, 1fr)',
+                }}>
+                    <Box>
+                        Property Owner: {owner_name}
+                    </Box>
+                    <Box>
+                        Contact: {owner_contact}
+                    </Box>
+                    <Box>
+                        Contract Start Date: {start_date}
+                    </Box>
+                    <Box>
+                        Contract End Date: {end_date}
+                    </Box>
+                    <Box>
+                        Email Address: {owner_email}
+                    </Box>
+                    <Box>
+                        Properties Included: 
+                    </Box>                    
+                </Box>
+            </Box>
+        );
+    }
+}
+
+function ApplicationCard(props) {
+    const [address, unit, city, state, zip, start_date, end_date, applicant, num_adults] = props.data;
     const navigate = useNavigate();
 
     function downloadDocument(url) {
@@ -370,13 +566,13 @@ function DocumentCard(props) {
                 <Box sx={{
                     fontWeight: 'bold',
                 }}>
-                    {document.filename}
+                    Application
                 </Box>
-                <Box sx={{
+                {/* <Box sx={{
                     fontWeight: 'bold',
                 }}>
                     Document type: {document.type}
-                </Box>
+                </Box> */}
                 <Box sx={{
                     display: 'flex',
                     justifyContent: 'space-between',
@@ -385,7 +581,7 @@ function DocumentCard(props) {
                         sx={{
                             marginRight: '3px',
                         }}
-                        onClick={() => { downloadDocument(document.link) }}
+                        // onClick={() => { downloadDocument(document.link) }}
                     >
                         <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M7 10.5L2.625 6.125L3.85 4.85625L6.125 7.13125V0H7.875V7.13125L10.15 4.85625L11.375 6.125L7 10.5ZM1.75 14C1.26875 14 0.856626 13.8285 0.513626 13.4855C0.170626 13.1425 -0.000581848 12.7307 1.48557e-06 12.25V9.625H1.75V12.25H12.25V9.625H14V12.25C14 12.7312 13.8285 13.1434 13.4855 13.4864C13.1425 13.8294 12.7307 14.0006 12.25 14H1.75Z" fill="#3D5CAC" />
@@ -401,25 +597,37 @@ function DocumentCard(props) {
                 </Box>
             </Box>
             <Box>
-                {`${address}, ${unit}`}
+                {`${address}, ${city}, ${state} ${zip} - Unit ${unit}`}
             </Box>
-            <Box sx={{
+            <Box>
+                    Created: {start_date? start_date : "<LEASE_START_DATE?>"}
+            </Box>
+            <Box>
+                    Applicant: {applicant}
+            </Box>
+            <Box>
+                    Occupancy: {num_adults} {"Adult(s)"}
+            </Box>
+            <Box>
+                    Authorization Background check:
+            </Box>
+            {/* <Box sx={{
                 display: 'grid',
                 gridTemplateColumns: 'repeat(2, 1fr)',
             }}>
                 <Box>
-                    Created: {start_date}
+                    Created: {start_date? start_date : "<START_DATE>"}
                 </Box>
                 <Box>
                     By: {"<Doolittle Managements>"}
                 </Box>
                 <Box>
-                    Expires: {end_date}
+                    Expires: {end_date? end_date : "<END_DATE>"}
                 </Box>
                 <Box>
                     For: {"<Kim Gordon>"}
                 </Box>
-            </Box>
+            </Box> */}
         </Box>
     );
 }
