138425d0701a4b35624ec101b81bd7250ab7db60
Ramin <Ramin.mousivand2@gmail.com>
2024-01-29 08:33:29 -0800

    Create the OwnerList component, and move the happiness matrix data into there, and reformat the matrix chart

--- src/components/ManagerDashboard/ManagerDashboard.jsx
+++ src/components/ManagerDashboard/ManagerDashboard.jsx
@@ -18,6 +18,7 @@ import AddRevenueIcon from '../../images/AddRevenueIcon.png'
 import LeaseWidget from "../Dashboard-Components/Lease/LeaseWidget";
 import Backdrop from "@mui/material/Backdrop"; 
 import CircularProgress from "@mui/material/CircularProgress";
+import OwnerList from "./OwnerList";
 
 const useStyles = makeStyles({
     button: {
@@ -131,8 +132,9 @@ function ManagerDashboard() {
                     </div>
 
                     <br />
-                    <div className="mt-widget-owner-happiness" onClick={() => navigate("/managerDashboardHappinessMatrix")}>
+                    <div className="mt-widget-owner-happiness" >
                         <h2 className="mt-expiry-widget-title"> Owner Happiness </h2>
+                        <OwnerList />
                     </div>
                     <br />
             
@@ -190,7 +192,7 @@ function ManagerDashboard() {
                             id="expense"
                             className={classes.button}
                             onClick={() => {
-                            navigate('/pmDocuments');
+                            navigate('/ownerDocuments');
                             }}
                             >
 
