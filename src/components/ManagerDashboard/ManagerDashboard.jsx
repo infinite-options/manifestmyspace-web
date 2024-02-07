c1f01caffdbce35084287602aa4f0d9399063261
Ramin <Ramin.mousivand2@gmail.com>
2024-02-07 15:27:22 -0800

    Remove a pointless call to contracts endpoint on the ManagerDashboard.jsx

--- src/components/ManagerDashboard/ManagerDashboard.jsx
+++ src/components/ManagerDashboard/ManagerDashboard.jsx
@@ -166,11 +166,9 @@ function ManagerDashboard() {
             setShowSpinner(true);
             const response = await fetch(`https://l0h6a9zi1e.execute-api.us-west-1.amazonaws.com/dev/dashboard/${getProfileId()}`)
             // const response = await fetch(`https://l0h6a9zi1e.execute-api.us-west-1.amazonaws.com/dev/dashboard/600-000003`)
-            const contractsResponse = await fetch(`https://l0h6a9zi1e.execute-api.us-west-1.amazonaws.com/dev/contracts/${getProfileId()}`)
 
             const propertiesResponse = await fetch(`https://l0h6a9zi1e.execute-api.us-west-1.amazonaws.com/dev/properties/${getProfileId()}`)
             try {
-                const contractsData = await contractsResponse.json();
                 const jsonData = await response.json()
                 const propertiesResponseJSON = await propertiesResponse.json()
                 setContractRequests(propertiesResponseJSON.NewPMRequests.result)
