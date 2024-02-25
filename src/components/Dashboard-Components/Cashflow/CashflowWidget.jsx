b0ca8b512224c85d7a1263d6f02691222624efd1
pmarathay <pmarathay@infiniteoptions.com>
2024-02-25 10:57:07 -0800

    modified Owner dashboard

--- src/components/Dashboard-Components/Cashflow/CashflowWidget.jsx
+++ src/components/Dashboard-Components/Cashflow/CashflowWidget.jsx
@@ -6,109 +6,113 @@ import theme from "../../../theme/theme";
 import MixedChart from "../../Graphs/OwnerCashflowGraph";
 import { months } from "moment";
 import { useUser } from "../../../contexts/UserContext";
-import Backdrop from "@mui/material/Backdrop"; 
+import Backdrop from "@mui/material/Backdrop";
 import CircularProgress from "@mui/material/CircularProgress";
 import { fetchCashflow, getTotalExpenseByMonthYear, getPast12MonthsCashflow, getTotalRevenueByMonthYear } from "../../Cashflow/CashflowFetchData";
 
-function CashflowWidget() {
-    const navigate = useNavigate();
+export default function CashflowWidget(props) {
+  const navigate = useNavigate();
+  let date = new Date();
+  let cashflowStatusData = props.cashflowData;
+  let cashflowRevenue = cashflowStatusData[0]?.amount;
+  let cashflowExpense = cashflowStatusData[1]?.amount;
+  let cashflow = cashflowRevenue - cashflowExpense;
+  console.log("Cashflow data available: ", cashflowStatusData);
+  console.log("Cashflow data available: ", cashflowStatusData[0]);
+  console.log("Cashflow data available: ", cashflowStatusData[0]?.cf_month);
+  console.log("Cashflow Revenue: ", cashflowRevenue);
+  console.log("Cashflow Expense: ", cashflowExpense);
 
-    let date = new Date();
-    let currentMonth = date.toLocaleString("default", { month: "long" });
-    let currentYear = date.getFullYear().toString();
-    
-    const [month, setMonth] = useState(currentMonth);
-    const [year, setYear] = useState(currentYear);
-    const { user, getProfileId } = useUser();
-    const [showSpinner, setShowSpinner] = useState(false);
-    const [totalRevenueByMonth, setTotalRevenueByMonth] = useState(0);
-    const [totalExpenseByMonth, setTotalExpenseByMonth] = useState(0);
-    const [revenueCashflowByMonth, setRevenueCashflowByMonth] = useState([]);
-    const [last12Months, setLast12Months] = useState([]);
-    const profileId = getProfileId();
-    const [cashflowData, setCashflowData] = useState(null);
+  let currentYear = date.getFullYear().toString();
+  let currentMonth = date.toLocaleString("default", { month: "long" });
 
-    useEffect(() => {
-        fetchCashflow(profileId).then((data) => {
-            setCashflowData(data)
-            let currentMonthYearRevenue = getTotalRevenueByMonthYear(data, currentMonth, currentYear)
-            let currentMonthYearExpense = getTotalExpenseByMonthYear(data, currentMonth, currentYear)
-            let last12months = getPast12MonthsCashflow(data, currentMonth, currentYear)
-            setTotalRevenueByMonth(currentMonthYearRevenue) // currently useing sum(total_paid)
-            setTotalExpenseByMonth(currentMonthYearExpense) // currently using sum(total_paid)
-            setLast12Months(last12months)
+  const [month, setMonth] = useState(currentMonth);
+  const [year, setYear] = useState(currentYear);
 
-        }).catch((error) => {
-            console.error("Error fetching cashflow data:", error)
-        })
-    }, [])
+  const { user, getProfileId } = useUser();
+  const profileId = getProfileId();
+  const [showSpinner, setShowSpinner] = useState(false);
 
-    return (
-        <ThemeProvider theme={theme}>
-        <Backdrop
-            sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-            open={showSpinner}
-        >
-            <CircularProgress color="inherit" />
-        </Backdrop>
-        <div className="cf-widget-main" onClick={() => navigate("/cashflow", {
-                state: {
-                    month,
-                    year,
-                }
-            } )}>
-            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-            <Grid item xs={6}>
-                <Stack direction="row" justifyContent="center">
-                <Typography sx={{ color: theme.typography.primary.black, fontWeight: theme.typography.primary.fontWeight, fontSize: theme.typography.largeFont }}>
-                    {month} {year}
-                </Typography>
-                </Stack>
-                <Box
-                component="span"
-                m={1}
-                padding={2}
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center"
-                style={{
-                    backgroundColor: theme.palette.custom.blue,
-                    borderRadius: "5px",
-                }}
-                >
-                <Typography sx={{ color: theme.typography.primary.black, fontWeight: theme.typography.primary.fontWeight }}>Cashflow</Typography>
-                <Typography sx={{ color: theme.typography.primary.black, fontWeight: theme.typography.primary.fontWeight }}>
-                    $
-                    {
-                    totalRevenueByMonth ? (totalRevenueByMonth - totalExpenseByMonth).toFixed(2) : "0.00"
-                    }
-                </Typography>
-                </Box>
-                <Box component="span" m={1} padding={2} display="flex" justifyContent="space-between" alignItems="center">
-                <Typography sx={{ color: theme.typography.primary.black, fontWeight: theme.typography.primary.fontWeight }}>Revenue</Typography>
-                <Typography sx={{ color: theme.typography.primary.black, fontWeight: theme.typography.primary.fontWeight }}>
-                    $
-                    {totalRevenueByMonth ? totalRevenueByMonth.toFixed(2) : "0.00"}
-                </Typography>
-                </Box>
-                <Box component="span" m={1} padding={2} display="flex" justifyContent="space-between" alignItems="center">
-                <Typography sx={{ color: theme.typography.primary.black, fontWeight: theme.typography.primary.fontWeight }}>Expenses</Typography>
-                <Typography sx={{ color: theme.typography.primary.black, fontWeight: theme.typography.primary.fontWeight }}>
-                    $
-                    {totalExpenseByMonth ? totalExpenseByMonth : "0.00"}
-                </Typography>
-                </Box>
-            </Grid>
-            <Grid item xs={6}>
-                <MixedChart 
-                    revenueCashflowByMonth={last12Months}
-                    activeButton={"Cashflow"}
-                />
-            </Grid>
-            </Grid>
-        </div>
-        </ThemeProvider>
-    );
-    }
+  const [totalRevenueByMonth, setTotalRevenueByMonth] = useState(0);
+  const [totalExpenseByMonth, setTotalExpenseByMonth] = useState(0);
+  const [revenueCashflowByMonth, setRevenueCashflowByMonth] = useState([]);
+  const [last12Months, setLast12Months] = useState([]);
+  const [cashflowData, setCashflowData] = useState(null);
 
-    export default CashflowWidget;
+  //   useEffect(() => {
+  //     fetchCashflow(profileId)
+  //       .then((data) => {
+  //         setCashflowData(data);
+  //         console.log("Returned Data: ", data);
+  //         let currentMonthYearRevenue = getTotalRevenueByMonthYear(data, currentMonth, currentYear);
+  //         let currentMonthYearExpense = getTotalExpenseByMonthYear(data, currentMonth, currentYear);
+  //         let last12months = getPast12MonthsCashflow(data, currentMonth, currentYear);
+  //         console.log("currentMonthYearRevenue: ", currentMonthYearRevenue);
+  //         console.log("currentMonthYearExpense: ", currentMonthYearExpense);
+  //         console.log("last12months: ", last12months);
+  //         setTotalRevenueByMonth(currentMonthYearRevenue); // currently useing sum(total_paid)
+  //         setTotalExpenseByMonth(currentMonthYearExpense); // currently using sum(total_paid)
+  //         setLast12Months(last12months);
+  //       })
+  //       .catch((error) => {
+  //         console.error("Error fetching cashflow data:", error);
+  //       });
+  //   }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Backdrop sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }} open={showSpinner}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <div
+        className="cf-widget-main"
+        onClick={() =>
+          navigate("/cashflow", {
+            state: {
+              month,
+              year,
+            },
+          })
+        }
+      >
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid item xs={6}>
+            <Stack direction="row" justifyContent="center">
+              <Typography sx={{ color: theme.typography.primary.black, fontWeight: theme.typography.primary.fontWeight, fontSize: theme.typography.largeFont }}>
+                {month} {year}
+              </Typography>
+            </Stack>
+            <Box
+              component="span"
+              m={1}
+              padding={2}
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              style={{
+                backgroundColor: theme.palette.custom.blue,
+                borderRadius: "5px",
+              }}
+            >
+              <Typography sx={{ color: theme.typography.primary.black, fontWeight: theme.typography.primary.fontWeight }}>Cashflow</Typography>
+              <Typography sx={{ color: theme.typography.primary.black, fontWeight: theme.typography.primary.fontWeight }}>${cashflow ? cashflow.toFixed(2) : "0.00"}</Typography>
+            </Box>
+            <Box component="span" m={1} padding={2} display="flex" justifyContent="space-between" alignItems="center">
+              <Typography sx={{ color: theme.typography.primary.black, fontWeight: theme.typography.primary.fontWeight }}>Revenue</Typography>
+              <Typography sx={{ color: theme.typography.primary.black, fontWeight: theme.typography.primary.fontWeight }}>${cashflowRevenue ? cashflowRevenue : "0.00"}</Typography>
+            </Box>
+            <Box component="span" m={1} padding={2} display="flex" justifyContent="space-between" alignItems="center">
+              <Typography sx={{ color: theme.typography.primary.black, fontWeight: theme.typography.primary.fontWeight }}>Expenses</Typography>
+              <Typography sx={{ color: theme.typography.primary.black, fontWeight: theme.typography.primary.fontWeight }}>${cashflowExpense ? cashflowExpense : "0.00"}</Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={6}>
+            <MixedChart revenueCashflowByMonth={last12Months} activeButton={"Cashflow"} />
+          </Grid>
+        </Grid>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+// export default CashflowWidget;
--- src/components/OwnerDashboard.jsx
+++ src/components/OwnerDashboard.jsx
@@ -1,112 +1,132 @@
 import { Chart } from "react-google-charts";
-import { Button, Box, ThemeProvider } from '@mui/material';
-import { PieChart, Pie, Legend, Cell } from 'recharts';
-import CashflowWidget from "./Dashboard-Components/Cashflow/CashflowWidget";
-import MaintenanceWidget from "./Dashboard-Components/Maintenance/MaintenanceWidget";
-import "../css/maintenance.css";
+import { Button, Box, ThemeProvider } from "@mui/material";
+import { PieChart, Pie, Legend, Cell } from "recharts";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import theme from "../theme/theme";
-import Dollar from '../images/Dollar.png'
-import File_dock_fill from '../images/File_dock_fill.png'
-import User_fill_dark from '../images/User_fill_dark.png'
-import { useUser } from "../contexts/UserContext";
-import PropertyRentWidget from "./Dashboard-Components/PropertyRent/PropertyRentWidget";
-import LeaseWidget from "./Dashboard-Components/Lease/LeaseWidget";
-import Backdrop from "@mui/material/Backdrop"; 
+import Backdrop from "@mui/material/Backdrop";
 import CircularProgress from "@mui/material/CircularProgress";
 
+import "../css/maintenance.css";
+import theme from "../theme/theme";
+import Dollar from "../images/Dollar.png";
+import File_dock_fill from "../images/File_dock_fill.png";
+import User_fill_dark from "../images/User_fill_dark.png";
+import { useUser } from "../contexts/UserContext";
+
+import CashflowWidget from "./Dashboard-Components/Cashflow/CashflowWidget";
+import MaintenanceWidget from "./Dashboard-Components/Maintenance/MaintenanceWidget";
+import PropertyRentWidget from "./Dashboard-Components/PropertyRent/PropertyRentWidget";
+import LeaseWidget from "./Dashboard-Components/Lease/LeaseWidget";
+
 export default function OwnerDashboard() {
+  console.log("Info in useUser: ", useUser());
+  const { user, getProfileId } = useUser(); // Not seeing what is in getProfileId
+  console.log("Profile ID: ", getProfileId);
+  const navigate = useNavigate();
+  console.log("Navigate Info: ", navigate); // Not seeing what is in navigate
+  let date = new Date();
 
-    const { user, getProfileId } = useUser();
-    const navigate = useNavigate();
-    let date = new Date();
-    // const [loading, setLoading] = useState(true);
-    const [rentStatus, setRentStatus] = useState([]);
-    const [leaseStatus, setLeaseStatus] = useState([]);
-    const [maintenanceStatusData, setMaintenanceStatusData] = useState([]);
-    const [showSpinner, setShowSpinner] = useState(false);
-    const [currentMonth, setCurrentMonth] = useState(date.getMonth()+1);
+  const [cashflowStatus, setCashflowStatus] = useState([]);
+  const [rentStatus, setRentStatus] = useState([]);
+  const [leaseStatus, setLeaseStatus] = useState([]);
+  const [maintenanceStatusData, setMaintenanceStatusData] = useState([]);
+  const [showSpinner, setShowSpinner] = useState(false);
+  const [currentMonth, setCurrentMonth] = useState(date.getMonth() + 1);
 
+  const [moveoutsInSixWeeks, setMoveoutsInSixWeeks] = useState(0);
+  const sliceColors = ["#A52A2A", "#FF8A00", "#FFC85C", "#160449", "#3D5CAC"];
+  const rentData = [
+    ["Properties", "Rent status"],
+    ["not paid", 18],
+    ["paid partially", 6],
+    ["paid late", 3],
+    ["vacant", 3],
+    ["paid on time", 36],
+  ];
 
-    const [moveoutsInSixWeeks, setMoveoutsInSixWeeks] = useState(0);
-    const sliceColors = ['#A52A2A', '#FF8A00', '#FFC85C', '#160449', '#3D5CAC'];
-    const rentData = [
-        ["Properties", "Rent status"],
-        ["not paid", 18],
-        ["paid partially", 6],
-        ["paid late", 3],
-        ["vacant", 3],
-        ["paid on time", 36],
-    ];
+  useEffect(() => {
+    const dataObject = {};
+    const fetchData = async () => {
+      console.log("Profile ID in UseEffect: ", getProfileId); // Not seeing what is in getProfileId
+      if (!getProfileId()) navigate("/PrivateprofileName");
+      setShowSpinner(true);
+      const response = await fetch(`https://l0h6a9zi1e.execute-api.us-west-1.amazonaws.com/dev/dashboard/${getProfileId()}`);
+      const jsonData = await response.json();
 
-    
-    useEffect(() => {
-        const dataObject = {};
-        const fetchData = async () => {
-            if (!getProfileId())
-            navigate('/PrivateprofileName')
-            setShowSpinner(true);
-            const response = await fetch(`https://l0h6a9zi1e.execute-api.us-west-1.amazonaws.com/dev/dashboard/${getProfileId()}`)
-            const jsonData = await response.json()
-            
-            // MAINTENANCE Status
-            setMaintenanceStatusData(jsonData.MaintenanceStatus.result);
+      // CASHFLOW Status  (PM - should this be in its own div below?)
+      setCashflowStatus(jsonData.CashflowStatus.result);
 
-            // RENT Status
-            setRentStatus(jsonData.RentStatus.result);
-            
-            // LEASE Status
-            setLeaseStatus(jsonData.LeaseStatus.result);
-            setShowSpinner(false);
-        }
-        fetchData();
-    }, []);
+      // MAINTENANCE Status  (PM - why have Data after MaintenanceStatus?)
+      setMaintenanceStatusData(jsonData.MaintenanceStatus.result);
 
+      // RENT Status
+      setRentStatus(jsonData.RentStatus.result);
 
-    // RETURN statement has HTML, CSS and uses all the DATA
-    return (
-        <ThemeProvider theme={theme}>
-            <Backdrop
-                sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-                open={showSpinner}
-            >
-                <CircularProgress color="inherit" />
-            </Backdrop>
+      // LEASE Status
+      setLeaseStatus(jsonData.LeaseStatus.result);
+      setShowSpinner(false);
+    };
+    fetchData();
+  }, []);
 
-                <div className="mt-widget-main">
-                    <CashflowWidget />
-                    <div className="mt-container">
-                        <MaintenanceWidget selectedRole={"OWNER"} maintenanceData={maintenanceStatusData}/>
-                        <PropertyRentWidget rentData={rentStatus}/>
-                    </div>
+  // RETURN statement has HTML, CSS and uses all the DATA
+  return (
+    <ThemeProvider theme={theme}>
+      <Backdrop sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }} open={showSpinner}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
 
-                    
-                    <div className="mt-container">
-                        {/* <LeaseWidget selectedRole={"OWNER"}/> */}
-                        <LeaseWidget leaseData={leaseStatus}/>
-                    </div>
+      <div className="mt-widget-main">
+        <CashflowWidget cashflowData={cashflowStatus} />
 
-                    <div className="bottom-buttons">
-                        <Button
-                            variant="outlined"
-                            id="revenue"
-                            className="bottom-item"
-                            onClick={() => { navigate('/transactionHistory') }}> <img src={Dollar}></img> Transactions</Button>
-                        <Button
-                            variant="outlined"
-                            id="expense"
-                            className="bottom-item"
-                            onClick={() => { navigate('/ownerDocuments') }}> <img src={File_dock_fill}></img> Documents</Button>
-                        <Button
-                            variant="outlined"
-                            id="maintenance"
-                            className="bottom-item"
-                            onClick={() => { navigate('/ownerContacts') }}> <img src={User_fill_dark}></img> Contacts</Button>
-                    </div>
-                    <br />
-                </div>
-            {/* } */}
-        </ThemeProvider>
-    )
+        <div className="mt-container">
+          <MaintenanceWidget selectedRole={"OWNER"} maintenanceData={maintenanceStatusData} />
+          <PropertyRentWidget rentData={rentStatus} />
+        </div>
+
+        <div className="mt-container">
+          {/* <LeaseWidget selectedRole={"OWNER"}/> */}
+          <LeaseWidget leaseData={leaseStatus} />
+        </div>
+
+        <div className="bottom-buttons">
+          <Button
+            variant="outlined"
+            id="revenue"
+            className="bottom-item"
+            onClick={() => {
+              navigate("/transactionHistory");
+            }}
+          >
+            {" "}
+            <img src={Dollar}></img> Transactions
+          </Button>
+          <Button
+            variant="outlined"
+            id="expense"
+            className="bottom-item"
+            onClick={() => {
+              navigate("/ownerDocuments");
+            }}
+          >
+            {" "}
+            <img src={File_dock_fill}></img> Documents
+          </Button>
+          <Button
+            variant="outlined"
+            id="maintenance"
+            className="bottom-item"
+            onClick={() => {
+              navigate("/ownerContacts");
+            }}
+          >
+            {" "}
+            <img src={User_fill_dark}></img> Contacts
+          </Button>
+        </div>
+        <br />
+      </div>
+      {/* } */}
+    </ThemeProvider>
+  );
 }
