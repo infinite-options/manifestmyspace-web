8ca27c37406f8add8d83eb9ccd5aa4c55753be58
Ranjit Marathay <rmarathay@gmail.com>
2024-04-04 09:10:32 -0700

    fix date time picker item

--- src/components/DateTimePicker.jsx
+++ src/components/DateTimePicker.jsx
@@ -41,7 +41,7 @@ function DateTimePickerModal(props) {
 
   function submit(){
     console.log("in submit for datetimepicker")
-    props.handleSubmit(availabilityDate, availabilityTime).then(
+    props.handleSubmit(props.maintenanceItem.maintenance_request_uid, availabilityDate, availabilityTime).then(
         handleClose()
     )
   }
