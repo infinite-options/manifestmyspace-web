d12670facbf0a27327ea73f9b7625085545266fc
Ramin <Ramin.mousivand2@gmail.com>
2024-04-07 17:18:53 -0700

    Address navigation bug on signup pages post-refresh; Redirect Maint_Employee to Supervisor Dashboard

--- src/components/ManagerDashboard/ManagerDashboard.jsx
+++ src/components/ManagerDashboard/ManagerDashboard.jsx
@@ -48,7 +48,7 @@ function ManagerDashboard() {
   const { getProfileId, user, selectedRole } = useUser();
   let dashboard_id=getProfileId()
   if (selectedRole==='PM_EMPLOYEE')
-  dashboard_id= user.businesses?.MANAGEMENT?.business_uid || user?.supervisor;
+    dashboard_id= user.businesses?.MANAGEMENT?.business_uid || user?.pm_supervisor;
   const navigate = useNavigate();
   let date = new Date();
   // const [loading, setLoading] = useState(true);
