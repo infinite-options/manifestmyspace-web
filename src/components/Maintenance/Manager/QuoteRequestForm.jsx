e0a47f88babd11c6e1037d694aa93b3d3d15d3da
Lasya Bheemendra Nalini <59815536+lasyabheemendra@users.noreply.github.com>
2024-06-11 12:42:01 -0700

    Back button position fix

--- src/components/Maintenance/Manager/QuoteRequestForm.jsx
+++ src/components/Maintenance/Manager/QuoteRequestForm.jsx
@@ -314,7 +314,7 @@ export default function QuoteRequestForm() {
 							paddingRight: '0px',
 						}}
 					>
-						<Box position="absolute" left={isMobile ? 30 : 480}>
+						<Box position="absolute" left="43%">
 							<Button onClick={() => handleBackButton()}>
 								<ArrowBackIcon
 									sx={{ color: theme.typography.primary.black, fontSize: '30px', margin: '5px' }}
