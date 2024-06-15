38d1c620d2404d81fcd3d546564d2479735f1c96
dhanooramnagaraj <dhanoonickson@gmail.com>
2024-06-15 08:02:26 -0400

    Updating AddressAutoComplete component to use UseJSAPILoader instead of LoadScript

--- src/components/Property/AddressAutocompleteInput.jsx
+++ src/components/Property/AddressAutocompleteInput.jsx
@@ -1,18 +1,15 @@
 import React, { useState, useRef, useCallback } from 'react';
-import { LoadScript, Autocomplete } from '@react-google-maps/api';
+import { useJsApiLoader, Autocomplete } from '@react-google-maps/api';
 
 const libraries = ['places'];
-
 const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 const AddressAutocompleteInput = ({ onAddressSelect, defaultValue, gray }) => {
-  // const [address, setAddress] = useState('');
-
   const autocomplete = useRef(null);
 
   const extractAddress = (components) => {
     const address = {
-      street: '',      
+      street: '',
       city: '',
       state: '',
       zip: '',
@@ -59,35 +56,64 @@ const AddressAutocompleteInput = ({ onAddressSelect, defaultValue, gray }) => {
     }
   };
 
+  const { isLoaded, loadError } = useJsApiLoader({
+    googleMapsApiKey: GOOGLE_API_KEY,
+    libraries,
+  });
+
+  if (loadError) {
+    return <div>Error loading Google Maps script</div>;
+  }
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <LoadScript googleMapsApiKey={GOOGLE_API_KEY} libraries={libraries} loading="async">      
-      <Autocomplete
-        onLoad={auto => { autocomplete.current = auto; }}
-        onPlaceChanged={onPlaceChanged}
-        options={{
-          types: ['address'],
-          componentRestrictions: { country: 'us' }, // Restrict search to the US          
-        }}
-      >
-        {gray ?<input
+    <Autocomplete
+      onLoad={auto => { autocomplete.current = auto; }}
+      onPlaceChanged={onPlaceChanged}
+      options={{
+        types: ['address'],
+        componentRestrictions: { country: 'us' },
+      }}
+    >
+      {gray ? (
+        <input
           type="text"
-          // placeholder={placeholder}
-          defaultValue={defaultValue? defaultValue : ""}
+          defaultValue={defaultValue ? defaultValue : ""}
           style={{
-          backgroundColor: "#D6D5DA",
-        borderRadius: 10, height: 30, marginBlock: 10, width:'100%', paddingBottom: "5px", outline:'none', border:'none'}}
-          onKeyDown={handleKeyDown}
-        /> : <input
-          type="text"
-          // placeholder={placeholder}
-          defaultValue={defaultValue? defaultValue : ""}
-          style={{ boxSizing: 'border-box', border: '1px solid transparent', width: '100%', height: '40px', padding: '0 12px', borderRadius: '3px', boxShadow: '0 2px 6px rgba(0, 0, 0, 0.3)', fontSize: '14px', outline: 'none', textOverflow: 'ellipses' }}
+            backgroundColor: "#D6D5DA",
+            borderRadius: 10,
+            height: 30,
+            marginBlock: 10,
+            width: '100%',
+            paddingBottom: "5px",
+            outline: 'none',
+            border: 'none'
+          }}
           onKeyDown={handleKeyDown}
         />
-        }
-      </Autocomplete>
-      {/* <p>DEBUG - Selected Address: {address}</p> */}
-    </LoadScript>
+      ) : (
+        <input
+          type="text"
+          defaultValue={defaultValue ? defaultValue : ""}
+          style={{
+            boxSizing: 'border-box',
+            border: '1px solid transparent',
+            width: '100%',
+            height: '40px',
+            padding: '0 12px',
+            borderRadius: '3px',
+            boxShadow: '0 2px 6px rgba(0, 0, 0, 0.3)',
+            fontSize: '14px',
+            outline: 'none',
+            textOverflow: 'ellipses'
+          }}
+          onKeyDown={handleKeyDown}
+        />
+      )}
+    </Autocomplete>
   );
 };
 
