6273b599e1db2f5b4c4c6fe5ed6aa52a825cddde
Lasya Bheemendra Nalini <59815536+lasyabheemendra@users.noreply.github.com>
2024-07-02 11:35:25 -0700

    Update WorkerMaintenanceRequestNavigator.jsx

--- src/components/Maintenance/Worker/WorkerMaintenanceRequestNavigator.jsx
+++ src/components/Maintenance/Worker/WorkerMaintenanceRequestNavigator.jsx
@@ -25,11 +25,8 @@ async function getInitialImages(requestData, currentIndex) {
 
 export default function WorkerMaintenanceRequestNavigator({ requestIndex, backward_active_status, forward_active_status, updateRequestIndex, requestData, color, item, allData, currentTabValue, status, tabs }) {
   console.log('----inside WorkerMaintenanceRequestNavigator----');
-  console.log('----requestIndex----', requestIndex);
   const [currentIndex, setCurrentIndex] = useState(requestIndex);
-  console.log('----currentIndex---', currentIndex);
   const [activeStep, setActiveStep] = useState(0);
-  console.log('----activeStep---', activeStep);
   const [formattedDate, setFormattedDate] = useState("");
   const [numOpenRequestDays, setNumOpenRequestDays] = useState("");
   const [images, setImages] = useState([maintenanceRequestImage]);
