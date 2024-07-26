efdd675c83b8648fbd6e18b45c8febdb6925eb81
pmarathay <pmarathay@infiniteoptions.com>
2024-07-26 15:16:30 -0700

    Started Dashboard Rewrite

--- src/components/ManagerDashboard/ManagerDashboard.jsx
+++ src/components/ManagerDashboard/ManagerDashboard.jsx
@@ -35,23 +35,76 @@ const useStyles = makeStyles({
 });
 
 function ManagerDashboard() {
-  // console.log("In Manager Dashboard function");
-  const { getProfileId, user, selectedRole } = useUser();
-  let dashboard_id = getProfileId();
-  if (selectedRole === "PM_EMPLOYEE") dashboard_id = user.businesses?.MANAGEMENT?.business_uid || user?.pm_supervisor;
-  const navigate = useNavigate();
-  // const [loading, setLoading] = useState(true);
+  console.log("In Manager Dashboard function");
 
+  const navigate = useNavigate();
+  const { getProfileId, user, selectedRole } = useUser();
   const [rentStatus, setRentStatus] = useState([]);
   const [leaseStatus, setLeaseStatus] = useState([]);
-  const [maintenanceStatusData, setMaintenanceStatusData] = useState([]);
-  const [showSpinner, setShowSpinner] = useState(true);
-  const [contractRequests, setContractRequests] = useState([]);
-  const [property_endpoint_resp, set_property_endpoint_resp] = useState([]);
   const [revenueData, setRevenueData] = useState([]);
   const [happinessData, setHappinessData] = useState([]);
-  const [dataforhappiness, setdataforhappiness] = useState([]);
+  const [maintenanceStatusData, setMaintenanceStatusData] = useState([]);
+  const [contractRequests, setContractRequests] = useState([]);
 
+  const [showSpinner, setShowSpinner] = useState(true);
+  const [showReferralWelcomeDialog, setShowReferralWelcomeDialog] = useState(false);
+  const sliceColors = ["#A52A2A", "#FF8A00", "#FFC85C", "#160449", "#3D5CAC"];
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const isMedium = useMediaQuery(theme.breakpoints.down("md"));
+
+  // Can we delete these?
+  const [dataforhappiness, setdataforhappiness] = useState([]);
+  const [matrixData, setMatrixData] = useState([]);
+  const [property_endpoint_resp, set_property_endpoint_resp] = useState([]);
+
+  //
+  //
+  // Check if No Profile ID or if Employee Profile ID or if some other Role
+  console.log("User Info: ", getProfileId(), selectedRole, user);
+
+  let dashboard_id = getProfileId();
+  if (selectedRole === "PM_EMPLOYEE") dashboard_id = user.businesses?.MANAGEMENT?.business_uid || user?.pm_supervisor;
+
+  if (!getProfileId()) {
+    let newRole = "MANAGER";
+    navigate("/addNewRole", { state: { user_uid: user.user_uid, newRole } });
+  }
+
+  // This should be done at User Login and Not on the Dashboard
+  // Employee Verification useEffect
+  useEffect(() => {
+    setShowSpinner(true);
+    if (selectedRole === "PM_EMPLOYEE") {
+      const emp_verification = async () => {
+        try {
+          const response = await fetch(`${APIConfig.baseURL.dev}/profile/${getProfileId()}`);
+          // const response = await fetch(`${APIConfig.baseURL.dev}/profile/600-000003`);
+          if (!response.ok) {
+            throw new Error("Failed to fetch data");
+          }
+          const data = await response.json();
+          const employee = data.result[0]; // Assuming there's only one employee
+          if (!employee?.employee_verification) {
+            navigate("/emp_waiting");
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+      emp_verification();
+      setShowSpinner(false);
+    }
+    const signedUpWithReferral = localStorage.getItem("signedUpWithReferral");
+    if (signedUpWithReferral && signedUpWithReferral === "true") {
+      setShowReferralWelcomeDialog(true);
+      localStorage.removeItem("signedUpWithReferral");
+    }
+  }, []);
+
+  //
+  //
+  // Console Logs for useState variables
   useEffect(() => {
     console.log("RentStatus check --", rentStatus);
   }, [rentStatus]);
@@ -64,14 +117,13 @@ function ManagerDashboard() {
     console.log("Contract requests - ", contractRequests);
   }, [contractRequests]);
 
-  const sliceColors = ["#A52A2A", "#FF8A00", "#FFC85C", "#160449", "#3D5CAC"];
-  const [showReferralWelcomeDialog, setShowReferralWelcomeDialog] = useState(false);
+  useEffect(() => {
+    console.log("Happiness Matrix Info - ", happinessData);
+  }, [happinessData]);
 
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-  const isMedium = useMediaQuery(theme.breakpoints.down("md"));
-
-  let [matrixData, setMatrixData] = useState([]);
-
+  //
+  //
+  // This should be done in Happiness Matrix Widget - Divya
   const setting_matrix_data = (happiness_response) => {
     // console.log("In Setting Happiness Matrix", happiness_response);
     // Transforming the data
@@ -156,50 +208,16 @@ function ManagerDashboard() {
 
     setMatrixData(transformedData);
   };
+  // End of Happiness Matrix Widget Info - Divya
 
-  // console.log("In Manager Dashboard Step 3");
-
-  // Employee Verification useEffect
-  useEffect(() => {
-    setShowSpinner(true);
-    if (selectedRole === "PM_EMPLOYEE") {
-      const emp_verification = async () => {
-        try {
-          const response = await fetch(`${APIConfig.baseURL.dev}/profile/${getProfileId()}`);
-          // const response = await fetch(`${APIConfig.baseURL.dev}/profile/600-000003`);
-          if (!response.ok) {
-            throw new Error("Failed to fetch data");
-          }
-          const data = await response.json();
-          const employee = data.result[0]; // Assuming there's only one employee
-          if (!employee?.employee_verification) {
-            navigate("/emp_waiting");
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      };
-
-      emp_verification();
-      setShowSpinner(false);
-    }
-    const signedUpWithReferral = localStorage.getItem("signedUpWithReferral");
-    if (signedUpWithReferral && signedUpWithReferral === "true") {
-      setShowReferralWelcomeDialog(true);
-      localStorage.removeItem("signedUpWithReferral");
-    }
-  }, []);
-
-  // USE EFFECT gets all the data
+  //
+  //
+  //  USE EFFECT gets all the data
   useEffect(() => {
     // const dataObject = {};
     // console.log("In UseEffect");
     // console.log(getProfileId());
 
-    console.log("user.user_uid is ", user)
-    if (!getProfileId()) 
-    { let newRole="MANAGER"
-      navigate("/addNewRole", { state: { user_uid: user.user_uid, newRole } });}
     // console.log("In UseEffect after if");
     const fetchData = async () => {
       setShowSpinner(true);
@@ -209,11 +227,7 @@ function ManagerDashboard() {
 
       try {
         const jsonData = await response.json();
-        // console.log("Manager Dashboard jsonData: ", jsonData);
-        // const propertiesResponseJSON = await propertiesResponse.json();
-
-        // MAINTENANCE Status
-        setMaintenanceStatusData(jsonData.MaintenanceStatus.result);
+        console.log("Manager Dashboard jsonData: ", jsonData);
 
         // RENT Status
         setRentStatus(jsonData.RentStatus.result);
@@ -221,18 +235,21 @@ function ManagerDashboard() {
         // LEASE Status
         setLeaseStatus(jsonData.LeaseStatus.result);
 
-        // HAPPINESS MATRIX
-        setting_matrix_data(jsonData);
-        setHappinessData(jsonData.HappinessMatrix);
-
-        // DATA FOR HAPPINESS
-        setdataforhappiness(jsonData);
-
         // REVENUE DATA
         setRevenueData(jsonData.Profitability);
 
-        // NEW PM REQUESTS
+        // HAPPINESS MATRIX
+        setHappinessData(jsonData.HappinessMatrix);
+
+        // DATA FOR HAPPINESS - Need to Remove this after Divya moves the Happiness Matrix Widget Info
+        setdataforhappiness(jsonData);
+        setting_matrix_data(jsonData);
         set_property_endpoint_resp(jsonData);
+
+        // MAINTENANCE Status
+        setMaintenanceStatusData(jsonData.MaintenanceStatus.result);
+
+        // NEW PM REQUESTS
         setContractRequests(jsonData.NewPMRequests.result);
       } catch (error) {
         console.error(error);
@@ -247,7 +264,7 @@ function ManagerDashboard() {
     return (
       <>
         <Backdrop sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }} open={showSpinner}>
-          <CircularProgress color="inherit" />
+          <CircularProgress color='inherit' />
         </Backdrop>
         <ShimmerUI />
       </>
@@ -256,7 +273,7 @@ function ManagerDashboard() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Container maxWidth="lg" sx={{ paddingTop: "10px", paddingBottom: "50px" }}>
+      <Container maxWidth='lg' sx={{ paddingTop: "10px", paddingBottom: "50px" }}>
         <Grid container spacing={6}>
           <Grid item xs={12}>
             <Box
@@ -281,7 +298,11 @@ function ManagerDashboard() {
             </Box>
           </Grid>
           <Grid item xs={12} md={3}>
-            <PropertyRentWidget rentData={rentStatus} propertyEndpointResp={property_endpoint_resp} contractRequests={contractRequests} />
+            <PropertyRentWidget
+              rentData={rentStatus}
+              contractRequests={contractRequests}
+              // propertyEndpointResp={property_endpoint_resp}
+            />
           </Grid>
           <Grid item xs={12} md={9}>
             <RevenueWidget revenueData={revenueData} />
@@ -290,12 +311,8 @@ function ManagerDashboard() {
               <Grid item xs={12} md={6}>
                 <HappinessMatrixWidget
                   happinessData={happinessData}
-                  data={matrixData}
-                  dataforhappiness={dataforhappiness}
-                  // cashflowData={cashflowData}
-                  // cashflowDetails={cashflowDetails}
-                  // cashflowDetailsByProperty={cashflowDetailsByProperty}
-                  // cashflowDetailsByPropertyByMonth={cashflowDetailsByPropertyByMonth}
+                  // data={matrixData}
+                  // dataforhappiness={dataforhappiness}
                 />
               </Grid>
               <Grid item xs={12} md={6} style={{ display: "flex", flexDirection: "row", justifyContent: "flex-end", alignItems: "flex-end" }}>
@@ -314,7 +331,7 @@ const ShimmerUI = () => {
   const isMedium = useMediaQuery(theme.breakpoints.down("md"));
   return (
     <ThemeProvider theme={theme}>
-      <Container maxWidth="lg" sx={{ paddingTop: "30px", paddingBottom: "50px" }}>
+      <Container maxWidth='lg' sx={{ paddingTop: "30px", paddingBottom: "50px" }}>
         <Grid container spacing={6}>
           <Grid item xs={12}>
             <Box
