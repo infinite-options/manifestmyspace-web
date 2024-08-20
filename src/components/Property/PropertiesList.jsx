1e359f5a6c9880c260986ed9b9b3986d78d4bb47
Lasya Bheemendra Nalini <59815536+lasyabheemendra@users.noreply.github.com>
2024-08-20 09:31:57 -0700

    Update PropertiesList.jsx

--- src/components/Property/PropertiesList.jsx
+++ src/components/Property/PropertiesList.jsx
@@ -141,10 +141,11 @@ export default function PropertiesList(props) {
     const imageArray = JSON.parse(property.property_images);
     if (property.property_favorite_image) {
       const index = imageArray.findIndex((image) => image === property.property_favorite_image);
-      console.log('---index---', index);
       if (index !== -1) {
         return property.property_favorite_image;
-      } else {
+      } else if (imageArray.length !== 0) {
+        return imageArray[0];
+      }else {
         return propertyImage;
       }
     } else if (imageArray.length !== 0) {
